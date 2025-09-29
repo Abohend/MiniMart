f823f856b5ab1fcd82e1696aba0a895093ff31b9
Mohamed Abohend <mhmdabohend@gmail.com>
2025-09-29 17:40:31 +0300

    initiate home page structure

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// load the navbar
+fetch("./components/header.html")
+.then(response => response.text())
+.then(data => {
+    document.querySelector('header').innerHTML = data;
+
+    // toggle navbar
+    const navToggle = document.querySelector('.nav-toggle');
+    navToggle.addEventListener('click', () => {
+        const navLinks = document.querySelector('.nav-links');
+        navLinks.classList.toggle('nav-links-visible');
+    });
+});
+
+// load the footer
+fetch("./components/footer.html")
+.then(response => response.text())
+.then(data => {
+    document.getElementById('footer').innerHTML = data;
+})
+
